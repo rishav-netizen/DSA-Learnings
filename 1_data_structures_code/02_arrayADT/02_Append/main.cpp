3b422fff79dc14ea7a1ab297d4a9f681b5ab4115
rishav-netizen <rishavkumar2k07@gmail.com>
2026-07-28 23:10:53 +0530

    modularity changes

--- 1_data_structures_code/02_arrayADT/02_Append/main.cpp
+++ 1_data_structures_code/02_arrayADT/02_Append/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef struct Array
+struct Array
 {
     int *A;
     int size;
     int length;
-} Array;
+};
 
-void Display(Array array);
+void Display(const Array &array);
 void Append(Array *array, int element);
+void Input(Array *array);
 
 int main(){
     
@@ -22,25 +22,39 @@ int main(){
     arr.size = n;
     arr.A = new int[n];
     arr.length = 0;
-    
-    int l;
-    cout << "Enter the number of elements in array: ";
-    cin >> l;
-    for (int i = 0; i < l; i++)
-    {
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> arr.A[i];
-    }
-    arr.length = l;
+
+    Input(&arr);
     
     Display(arr);
     Append(&arr, 69);
     Display(arr);
     
+    delete[] arr.A;
     return 0;
 }
 
-void Display(Array array)
+void Input(Array *array)
+{
+    int length;
+    cout << "Enter the number of elements in array: ";
+    cin >> length;
+
+    while (length < 0 || length > array->size)
+    {
+        cout << "Invalid input! Number of elements must be between 0 and the array size.\nTry again: ";
+        cin >> length;
+    }
+
+    array->length = length;
+
+    for (int i = 0; i < array->length; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> array->A[i];
+    }
+}
+
+void Display(const Array &array)
 {
     cout << "The array is : {";
     for (int i = 0; i < array.length; i++)
@@ -56,9 +70,13 @@ void Display(Array array)
 void Append(Array *array, int element)
 {
     // array->length < array->size
-    if ((*array).length < (*array).size)
+    if (array->length < array->size)
     {
         array->A[array->length++] = element; 
+    }
+    else
+    {
+        cout << "Array is full! Cannot append element.\n";
     }
 
 }   

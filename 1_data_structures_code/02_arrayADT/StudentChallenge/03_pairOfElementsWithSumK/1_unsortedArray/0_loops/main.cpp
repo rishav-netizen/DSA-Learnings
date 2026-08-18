ea5e7938a6abd554487b165ab4f6e7996250373f
rishav-netizen <rishavkumar2k07@gmail.com>
2026-08-19 00:37:07 +0530

    loops approach for the same

---
+++ 1_data_structures_code/02_arrayADT/StudentChallenge/03_pairOfElementsWithSumK/1_unsortedArray/0_loops/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+
+using namespace std;
+
+class Array
+{
+    private:
+        int *A;
+        int size;
+        int length;
+    
+    public:
+        Array()
+        {
+            size = 10;
+            A = new int[size];
+            length = 0; 
+        }
+        Array(int sz)
+        {
+            size = sz;
+            A = new int[size];
+            length = 0; 
+        }
+        ~Array()
+        {
+            cout << "Instance of array object destructed!\n";
+            delete[] A;
+            A = nullptr;
+        }
+        // Similarly we can add other functions
+        void Display();
+        void Insert(int index, int x);
+        int Delete(int index);
+        void Input();
+        int Max();
+        void ElementsWithSum(int sum);
+        void DuplicatesWithCount();
+    };
+
+void Array::Display()
+{
+    cout << "Elements of the array are: {";
+    for (int i = 0; i < length; i++)
+    {   
+        cout << A[i];
+        if (i < length - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << "}\n";
+}
+
+void Array::Insert(int index, int x)
+{
+    if ((index >= 0) and (index <= length) and (length < size))
+    {
+        for (int i = length - 1; i >= index; i--)
+        {
+            A[i + 1] = A[i];
+        }
+        A[index] = x;
+        length++;
+    }
+}
+
+
+int Array::Delete(int index)
+{
+    int deleted = -1;
+    if ((index >= 0) and (index < length))
+    {
+        deleted = A[index];
+        for (int i = index; i < length - 1; i++)
+        {
+            A[i] = A[i + 1];
+        }
+        length--;
+    }
+    return deleted;
+}
+
+void Array::Input()
+{
+    cout << "Enter the number of numbers: ";
+    cin >> length;
+    while (length < 0 || length > size)
+    {
+        cout << "Invalid input! Length must be smaller than size!" << endl;
+        cout << "Try again: ";
+        cin >> length;
+    }
+    for (int i = 0, l = length; i < l; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> A[i];
+    }
+
+}
+
+int Array::Max()
+{
+    int result = A[0];
+    for (int i = 1; i < length; i++)
+    {
+        result = (result > A[i]) ? result : A[i];
+    }
+    return result;
+    
+}
+
+// iterative approach gives O(n^2) time complexity
+void Array::ElementsWithSum(int sum)
+{
+    bool found = false;
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = i + 1; j < length; j++)
+        {
+            if (A[i] + A[j] == sum)
+            {
+                cout << A[i] << " + " << A[j] << " = " << sum << endl;
+                found = true;
+            }
+        }
+    }
+
+    if (!found)
+    {
+        cout << "No elements found with that sum!\n";
+    }
+    
+}
+
+int main()
+{
+    Array A;
+    A.Input();
+    A.Display();
+    int n;
+    cout << "Enter sum: ";
+    cin >> n;
+    A.ElementsWithSum(n);
+    return 0;   
+}

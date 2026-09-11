f548851cf1819420501e697b65b3078ccaf992f9
rishav-netizen <rishavkumar2k07@gmail.com>
2026-09-12 02:17:51 +0530

    added code for column major lower triangular matrix

---
+++ 1_data_structures_code/04_matrices/02_lower_triangular_matrix/02_column_major/lower_triangular_matrix.cpp
@@ -0,0 +1,101 @@
+// Column Major Lower Triangular Matrix Implementation Placeholder
+#include <iostream>
+
+using namespace std;
+
+//? (i, j) are the matrix coordinates hence they start from 1
+//? the array we use to store the elements are 0 indexed
+//? for COLUMN MAJOR: i >= j
+
+class LowerTriangularMatrix
+{
+    private:
+        int n;
+        int *A;
+    
+    public:
+        LowerTriangularMatrix(int n)
+        {
+            this->n = n;
+            A = new int[n*(n+1)/2]();
+        }
+
+        void set(int i, int j, int value)
+        {
+            //! if we use this formula, which can be easily noticed
+            //? aij = (sum_till(n) - sum_till(n-j+1)) + i - j 
+            if (i >= j)
+            {
+                A[(n*(n+1)/2 - (n-j+1)*(n-j+2)/2) + (i-j)] = value;
+            }
+        }
+
+        int get(int i, int j)
+        {
+            if (i >= j)
+            {
+                return A[(n*(n+1)/2 - (n-j+1)*(n-j+2)/2) + (i-j)];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        void display()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    cout << '\t';
+                    if (i >= j)
+                    {
+                        cout << A[(n*(n+1)/2 - (n-j+1)*(n-j+2)/2) + (i-j)];
+                    }
+                    else
+                    {
+                        cout << '0';
+                    }
+                    cout << ' ';
+                }
+                cout << '\n';
+                
+            }
+            
+        }
+
+        int getDimension()
+        {
+            return n;
+        }
+
+        ~LowerTriangularMatrix()
+        {
+            delete[] A;
+            A = nullptr;
+        }
+};
+
+int main()
+{
+    int n;
+    cout << "n: ";
+    cin >> n;
+    LowerTriangularMatrix m(n);
+
+    cout << "Column Major Lower Triangular Matrix: \n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            m.set(i, j, i * j + 1);
+        }
+
+    }
+    
+    m.display();
+    return 0;
+}
+
+
